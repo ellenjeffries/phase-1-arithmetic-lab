fd7fd8e110abb620783cca92f8296fbbc25dc167
ellen <ellenjojeffries@gmail.com>
2024-05-31 15:21:21 -0600

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()*100)+ 1
+
+let num3 = 16
+let num4 = 6
+let mod = num3 %= num4
+
+
+let set = (1,5,9,14,20)
+let max = Math.max(set)
